35ea41d0bfe9d1d1cd8101c255345752b528ae02
kavin123-git <153710423+kavin123-git@users.noreply.github.com>
2024-05-28 11:13:08 +0530

    Create 20.Valid_Parentheses.cpp

---
+++ Assignment 2/20.Valid_Parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.length();
+       stack<char> st;
+        for(auto i:s)
+        {
+            if(i=='(' || i=='{'|| i=='[' )
+            {
+                st.push(i);
+            }
+            else
+            {
+                if(st.empty() || (st.top()=='(' && i!=')') ||(st.top()=='{' && i!='}')||(st.top()=='[' && i!=']'))
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+            
+        }
+        return st.empty();
+
+    }
+};
