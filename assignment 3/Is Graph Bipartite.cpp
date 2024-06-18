d8611a8be4b89fb4926a9ad87139141ab467bcb5
kavin123-git <153710423+kavin123-git@users.noreply.github.com>
2024-06-18 11:38:59 +0530

    Create Is Graph Bipartite.cpp

---
+++ assignment 3/Is Graph Bipartite.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool dfs(vector<int>adj[], int node,vector<int>&vis){
+        for(auto it: adj[node]){
+            if(vis[it]==-1){
+                vis[it]= !vis[node];
+                if(!dfs(adj,it,vis))return false;
+            }
+            else if(vis[it]==vis[node])return false; 
+        }
+        return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                adj[i].push_back(it);
+            }
+        }
+        vector<int>vis(n,-1);
+        for(int i=0;i<n;i++){
+            if(vis[i]==-1){
+                vis[i] = 0;
+                bool ans = dfs(adj,i,vis);
+                if(!ans)return false;
+            }
+        }
+        return true;
+    }
+};
